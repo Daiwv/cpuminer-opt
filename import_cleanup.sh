d89577448d3480fb508c148cc7a4b86e8ad88dfa
Eugene Bujak <hmage@hmage.net>
2017-01-16 21:08:57 +0300

    Update import cleanup script

--- import_cleanup.sh
+++ import_cleanup.sh
@@ -11,5 +11,6 @@ find . -name '*.del' -print0 | xargs -0 rm -rvf
 find . -name '*.swo' -print0 | xargs -0 rm -rvf
 find . -name '*.swp' -print0 | xargs -0 rm -rvf
 find . -name '*.wtf' -print0 | xargs -0 rm -rvf
+find . -name '*.broke' -print0 | xargs -0 rm -rvf
 find . -name '*.log' -print0 | xargs -0 rm -rvf
 find . -name .dirstamp -print0 | xargs -0 rm -rvf
